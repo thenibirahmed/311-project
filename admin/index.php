535d03a87ef9f301c4ed1edeee7962ce9276dbd3
Nibir Ahmed <nibirahmed9611@gmail.com>
2022-04-19 23:02:16 +0600

    dashboard functionality added

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,49 @@
     <!-- Begin Page Content -->
     <div class="container-fluid">
         <!-- Page Heading -->
-        <h1 class="h3 mb-4 text-gray-800">All Tours</h1>
+        <h1 class="h3 mb-4 text-gray-800">Welcome to NSU Traveling Zone Admin</h1>
+        <div class="row">
+            <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['logged_in_user']['role'] == 'admin' ) :  ?>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h5 class="card-header">All Packeges</h5>
+                        <div class="card-body">
+                            <p class="card-title">See all tour packages</p>
+                            <a href="all-tours.php" class="btn btn-primary">See Tour</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h5 class="card-header">Add Package</h5>
+                        <div class="card-body">
+                            <p class="card-title">Add a tour package</p>
+                            <a href="add-tour.php" class="btn btn-primary">Add Tour</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h5 class="card-header">All Bookings</h5>
+                        <div class="card-body">
+                            <p class="card-title">See and manage all bookings</p>
+                            <a href="all-bookings.php" class="btn btn-primary">See Bookings</a>
+                        </div>
+                    </div>
+                </div>
+            <?php else: ?>
+                <div class="col-md-4">
+                    <div class="card">
+                        <h5 class="card-header">My Bookings</h5>
+                        <div class="card-body">
+                            <p class="card-title">See my bookings</p>
+                            <a href="all-bookings.php" class="btn btn-primary">See Bookings</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+        
 
         
     </div>
